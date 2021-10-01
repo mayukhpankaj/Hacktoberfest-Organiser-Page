088504cb70be9cc148e00700c5c9a90c9c35b0ee
Mayukh Pankaj <mayukhpankaj30@gmail.com>
2021-10-01 15:26:33 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+//Edit your details.
+
+//add your github username and Hactoberfest Repository for showing contributions ! 
+
+const orgname = "Your Organisation";
+const message ="Your Tagline !"
+const logo_url = 'img/github.png';  //your logo url  -> img/filename.extension
+const github_username ="github"   //your username
+const github_reponame = "rest-api-description"; //your reponame
+const linkedin = "mayukh-pankaj"    // your linkedin Username
+const instagram ="may.code"     //your Insta Id
+const facebook = "maycode"      //your Insta id
